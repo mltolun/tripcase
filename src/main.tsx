dba1128cbc174d1f7f2ea4f3b1a30f4aa83330c8
Martin Luna <martin.luna@gmail.com>
2026-05-26 16:24:42 +0200

    vite domain fix

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/tripcase">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
